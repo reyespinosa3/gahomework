d11e7f8c850f5ecc89a7c1f90c77bd256206da2a
Rey Espinosa <reynaldo.espinosa.III@gmail.com>
2018-03-22 19:48:44 -0500

    Commit 1 - Verbal questions

---
+++ week1/day4/soultions.js
@@ -0,0 +1,7 @@
+console.log("Ready to go");
+
+// What is the difference between a PARAMETER and an ARGUEMENT?
+// A Parameter is the variable in a function. An Arguement is the actual value of the variable the function the function will use.
+
+// What is the difference between RETURN and CONSOLE.LOG?
+// RETURN will return a value to the function. CONSOLE.LOG will output a message to the console (used for debugging code).
