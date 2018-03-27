4ab636df5d0cbed117e842c21ef579d749c6c99e
Rey Espinosa <reynaldo.espinosa.III@gmail.com>
2018-03-26 21:53:17 -0500

    2nd commit - created some data structures and accessed some data

---
+++ week2/day1/answers.js
@@ -0,0 +1,24 @@
+// 1. light switch = boolean
+//      on or off -- true or false -- 0 or 1
+// 2. email address = string
+//      reynaldo.espinosa.iii@gmail.com
+// 3. spaceship parts = array of strings
+//      ["hull", "laser blasters", "tractor beam", "warp drive"]
+// 4. student names list = array of strings
+//      ["Jaime", "Spencer", "VP"]
+// 5. student names and locations = object
+//      {name: "Jeremy", location: "Austin"}
+// 6. student names, location and fav TV show = object
+//      {name: "Jeremy", location: "Austin", favTV: "Office"}
+
+//Take it Easy
+const rainbow = ["Red", "Orange", "Yellow", "Blue", "Green", "Violet"];
+console.log(rainbow[3]);
+
+const rey = {
+  favFood: "tacos",
+  hobby: "camping",
+  town: "Manor",
+  favDataType: "boolean"
+}
+console.log(rey.hobby);
