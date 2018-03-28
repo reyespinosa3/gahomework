90a881e72ad3655ffe8b4fa392433047f7bc95d4
Rey Espinosa <reynaldo.espinosa.III@gmail.com>
2018-03-27 19:58:01 -0500

    Created initial js, css and html files. Created Micro Blog title and h1 tag.

--- week2/day1/answers.js
+++ week2/day1/answers.js
@@ -49,7 +49,7 @@ const crazyObject = {
     ]
   }
 }
-console.log(crazyObject.taco[0].salsa[5]);
+console.log(crazyObject.taco[1].salsa[5]);
 console.log(crazyObject.larry.quotes[0]);
 console.log(crazyObject.larry.characters[2].favourtieHobby);
 console.log(crazyObject.larry.nicknames[1]);
